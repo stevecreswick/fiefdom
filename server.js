81f2d7cb6da992fa43217db5ec19058dec2a6d2e
Steve Creswick <screswick@gmail.com>
2017-12-01 19:15:05 -0500

    add index route to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/src/index.html'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080);
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
