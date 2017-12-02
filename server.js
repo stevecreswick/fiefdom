50d21a7c7e039009bf02b69ad0313dd50c3fba2d
Steve Creswick <screswick@gmail.com>
2017-12-01 19:09:21 -0500

    Point to index html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src/index.html'));
 
 app.listen(process.env.PORT || 8080);
